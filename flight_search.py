3bec037d5026e2386f8b9da666c5bb7931478d6d
Obiora Okafor <obioracelestine@gmail.com>
2022-11-04 19:16:14 +0100

    Update flight_search.py

--- flight_search.py
+++ flight_search.py
@@ -9,6 +9,7 @@ dotenv.load_dotenv(dotenv_file)
 
 
 class FlightSearch:
+    # This class handles flight searches for the destination cities provided
     def __init__(self):
         self.flight_search_endpoint = os.environ.get("FLIGHTSEARCH_ENDPOINT")
         self.return_ticket = {"apikey": os.environ.get("RETURN_TICKET")}
@@ -20,6 +21,7 @@ class FlightSearch:
         self.currency = "NGN"
 
     def search_flight(self, departure, destination):
+        """This method performs flight searches from the flight API and sends the result to main.py"""
         parameters = {
             "fly_from": departure,
             "fly_to": destination,
