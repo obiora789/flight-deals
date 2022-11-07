3d17293c0251b9d29854e860b70a4d5d25030957
Obiora Okafor <obioracelestine@gmail.com>
2022-11-07 04:42:26 +0100

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -6,9 +6,7 @@ import os
 dotenv_file = dotenv.find_dotenv()
 dotenv.load_dotenv(dotenv_file)
 
-
 class DataManager:
-    # This class manages all data to and from Google sheets as well as updates its local database
     def __init__(self):
         self.sheety_endpoint = os.environ.get("SHEETY_ENDPOINT")
         self.sheety_auth = {"Authorization": "Bearer " + os.getenv("SHEETY_AUTH")}
@@ -24,15 +22,19 @@ class DataManager:
     def write_google_sheet(self, sheety_list):
         """This method updates the Google sheet with flight details """
         price_upload = {}
-        items = [{"iataCode": sheet_dict["iataCode"], "icaoCode": sheet_dict["icaoCode"],
-                  "lowestPrice": f"₦{sheet_dict['lowestPrice']}", "id": sheet_dict["id"]}
-                 for sheet_dict in sheety_list]
-
-        for item in items:
-            price_upload["cost"] = item
-            sheety_write = requests.put(
-                url=f"{self.sheety_endpoint}/{item['id']}", json=price_upload, headers=self.sheety_auth)
-            sheety_write.raise_for_status()
+        try:
+            items = [{"iataCode": sheet_dict["iataCode"], "icaoCode": sheet_dict["icaoCode"],
+                      "lowestPrice": f"₦{sheet_dict['lowestPrice']}", "id": sheet_dict["id"]}
+                     for sheet_dict in sheety_list]
+        except KeyError:
+            items = [{"lowestPrice": f"₦{sheet_dict['lowestPrice']}", "id": sheet_dict["id"]}
+                     for sheet_dict in sheety_list]
+        else:
+            for item in items:
+                price_upload["cost"] = item
+                sheety_write = requests.put(
+                    url=f"{self.sheety_endpoint}/{item['id']}", json=price_upload, headers=self.sheety_auth)
+                sheety_write.raise_for_status()
 
     def search_local(self, city: str):
         """This method performs a local search through the data.json. It only searches the flight API for
